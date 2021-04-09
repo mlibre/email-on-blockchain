92163d1552c9963abb447a3b91362b95c5030274
mlibre <m.gh@linuxmail.org>
2021-04-09 15:37:35 +0430

    adding jquery, ipcman

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-const { app, BrowserWindow, screen, Menu } = require("electron");
+const { app, BrowserWindow, screen, Menu, ipcMain } = require("electron");
 const path = require("path");
 
 // require("electron-reload")(__dirname);
 
-Menu.setApplicationMenu(null);
+// Menu.setApplicationMenu(null);
+
+ipcMain.on("asynchronous-message", (event, arg) => 
+{
+	console.log(arg);
+
+	// Event emitter for sending asynchronous messages
+	event.sender.send("asynchronous-reply", "async pong");
+});
+
+// Event handler for synchronous incoming messages
+ipcMain.on("synchronous-message", (event, arg) => 
+{
+	console.log(arg); 
+
+	// Synchronous event emmision
+	event.returnValue = "sync pong";
+});
+
 function createWindow (width, height) 
 {
 	const win = new BrowserWindow({
@@ -15,7 +33,10 @@ function createWindow (width, height)
 		// titleBarStyle: "hiddenInset",
 		icon: `${__dirname}/ui/logo.png`,
 		webPreferences: {
-			preload: path.join(__dirname, "preload.js")
+			preload: path.join(__dirname, "preload.js"),
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true
 		}
 	});
 
